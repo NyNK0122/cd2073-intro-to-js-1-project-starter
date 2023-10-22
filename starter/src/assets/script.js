5781e580e673cf6b1b6f52f2d3935b5fad196545
NyNK <nknys2s9@gmail.com>
2023-10-22 17:20:56 +0700

    shopping-cart

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,5 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+const products = [];
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -8,6 +9,28 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+const cherry = {
+  name: "Carton of Cherries",
+  price: 4,
+  quantity: 0,
+  productId: 1,
+  image: "./images/cherry.jpg",
+};
+const strawberry = {
+  name: "Carton of Strawberries",
+  price: 5,
+  quantity: 0,
+  productId: 2,
+  image: "./images/strawberry.jpg",
+};
+const orange = {
+  name: "Bag of Oranges",
+  price: 10,
+  quantity: 0,
+  productId: 3,
+  image: "./images/orange.jpg",
+};
+products.push(cherry, strawberry, orange);
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
@@ -16,37 +39,89 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+const cart = [];
+
+// Get product by productId
+function selectedProduct(productId) {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].productId === productId) {
+      return products[i];
+    }
+  }
+}
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId) {
+  const chosenProduct = selectedProduct(productId);
+  if (cart.indexOf(chosenProduct) === -1) {
+    cart.push(chosenProduct);
+    chosenProduct.quantity += 1;
+  } else {
+    chosenProduct.quantity += 1;
+  }
+}
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId) {
+  const chosenProduct = selectedProduct(productId);
+  chosenProduct.quantity += 1;
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId) {
+  const chosenProduct = selectedProduct(productId);
+  if (chosenProduct.quantity === 1) {
+    removeProductFromCart(productId);
+  } else {
+    chosenProduct.quantity -= 1;
+  }
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart(productId) {
+  const chosenProduct = selectedProduct(productId);
+  chosenProduct.quantity = 0;
+  cart.splice(
+    cart.findIndex((product) => product === chosenProduct),
+    1
+  );
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total cost of all products
   - cartTotal should return the total cost of the products in the cart
   Hint: price and quantity can be used to determine total cost
 */
+function cartTotal() {
+  let grandTotal = 0;
+  cart.forEach((product) => {
+    grandTotal += product.quantity * product.price;
+  });
+  return grandTotal;
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
+function emptyCart() {
+  cart.forEach((product) => {
+    product.quantity = 0;
+  });
+  cart.splice(0, cart.length);
+}
 
 /* Create a function named pay that takes in an amount as an argument
   - amount is the money paid by customer
@@ -54,6 +129,17 @@
   - pay will return a positive number if money should be returned to customer
   Hint: cartTotal function gives us cost of all the products in the cart  
 */
+let totalPaid = 0;
+function pay(amount) {
+  totalPaid += amount;
+  returnedCash = totalPaid - cartTotal();
+  if (returnedCash < 0) {
+    return returnedCash;
+  } else {
+    totalPaid = 0;
+    return returnedCash;
+  }
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
@@ -64,16 +150,18 @@
    npm run test
 */
 
-module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
+if (typeof module === "object") {
+  module.exports = {
+    products,
+    cart,
+    addProductToCart,
+    increaseQuantity,
+    decreaseQuantity,
+    removeProductFromCart,
+    cartTotal,
+    pay,
+    emptyCart,
+    /* Uncomment the following line if completing the currency converter bonus */
+    // currency
+  };
 }
